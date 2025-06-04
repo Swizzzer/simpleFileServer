9af69caf66c7768e5d96b8eb442dcd843a6dbda9
Swizzer <kyoshaft@outlook.com>
2025-06-04 21:36:30 +0800

    chore: a detailed test script

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# first run the server
+# then use `dd` to create a 10GB file
+# dd if=/dev/zero of=10gbfile bs=1g count=10
+
 # https://github.com/n-WN/share_these/blob/main/test/test.sh
 for i in {1..10}; do
     offset=$(( (i-1) * 1073741824 ))  # 10GB/10 = 1GB per client
