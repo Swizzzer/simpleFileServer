74d7b62bf8990df922d3a3084e0d394b5375aee8
Swizzer <kyoshaft@outlook.com>
2025-06-07 00:07:16 +0800

    release: bump version to v0.2.1

--- src/log.rs
+++ src/log.rs
@@ -106,7 +106,7 @@ pub fn banner(args: &Args, serve_dir: &std::path::Path) {
     println!(
         "{} {}",
         "🚀 Swizzer's HTTP File Server".bright_white().bold(),
-        "v0.2.0".bright_black()
+        "v0.2.1".bright_black()
     );
     println!(
         "{}",
